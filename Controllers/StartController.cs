279246f7ea3cd383eb72d74970bdb627e453e12a
Alina Garifjanova <alina.garifjanova@yh.nackademin.se>
2024-09-20 14:40:10 +0200

    Updated pages and views for umbraco

---
+++ Controllers/StartController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.Controllers;
+using Umbraco.Cms.Web.Common.PublishedModels;
+using UmbracoProject.Models.ViewModels;
+
+namespace UmbracoProject.Controllers;
+
+public class StartController : RenderController
+{
+    private readonly IUmbracoContextAccessor _umbracoContextAccessor;
+	private readonly IPublishedContentQuery _publishContentQuery;
+
+	public StartController(ILogger<RenderController> logger, ICompositeViewEngine compositeViewEngine, IUmbracoContextAccessor umbracoContextAccessor, IPublishedContentQuery publishedContentQuery) : base(logger, compositeViewEngine, umbracoContextAccessor)
+    {
+        _umbracoContextAccessor = umbracoContextAccessor;
+        _publishContentQuery = publishedContentQuery;
+    }
+
+    public override IActionResult Index()
+    {
+        var startPage = CurrentPage as Start;
+        if (startPage != null)
+        {
+            var model = new StartPageViewModel(startPage, _umbracoContextAccessor, _publishContentQuery);
+          
+            return View(nameof(Start).ToLower(), model);
+        }
+       
+        return null!;
+    }
+}
---
+++ Models/ViewModels/StartPageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Web.Common.PublishedModels;
+
+namespace UmbracoProject.Models.ViewModels;
+
+public class StartPageViewModel : BasePageModel<Start>
+{
+    public StartPageViewModel(Start content, IUmbracoContextAccessor umbracoContextAccessor, IPublishedContentQuery contentQuery) : base(content, umbracoContextAccessor, contentQuery)
+    {
+    }
+}
